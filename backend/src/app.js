1665469b63a8f302dea7569329ad010dff0cda26
doctorbond2 <michael.rostlund@hotmail.com>
2024-03-27 22:51:15 +0100

    server still working fine :)

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,9 +6,7 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
-  res.send(
-    `hi there. server is working\n PORT: ${process.env.PORT}. \n MONGODB_URI: ${process.env.MONGODB_URI}`
-  );
+  res.send(`hi there. server is working fine! `);
 });
 app.use("/api/user", userRouter);
 export default app;
