c666cfcb710c50e010473405d1e31268d44ab240
doctorbond2 <michael.rostlund@hotmail.com>
2024-03-27 15:25:52 +0100

    update mongodb URI

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,6 +6,8 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
-  res.send("hi there");
+  res.send(
+    `hi there. server is working\n PORT: ${process.env.PORT}. \n MONGODB_URI: ${process.env.MONGODB_URI}`
+  );
 });
 export default app;
