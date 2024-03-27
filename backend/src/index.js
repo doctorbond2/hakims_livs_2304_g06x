3f1d6ac481e18fb4ddf73296d0fe4b24d6673aff
doctorbond2 <michael.rostlund@hotmail.com>
2024-03-27 13:26:42 +0100

    try again

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,10 @@
-import app from "./app.js";
-// import { RUN_SERVER } from "./config/dbconfig.js";
-const PORT = process.env.PORT || 3000;
-const MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb://localhost:27017/hakims_livs";
-// RUN_SERVER(MONGODB_URI, PORT);
-app.listen(process.env.PORT, () => {
-  console.log("Connected to host:", process.env.PORT);
-});
+// import app from "./app.js";
+// // import { RUN_SERVER } from "./config/dbconfig.js";
+// const PORT = process.env.PORT || 3000;
+// const MONGODB_URI =
+//   process.env.MONGODB_URI || "mongodb://localhost:27017/hakims_livs";
+// // RUN_SERVER(MONGODB_URI, PORT);
+// app.listen(process.env.PORT, () => {
+//   console.log("Connected to host:", process.env.PORT);
+// });
+console.log(12);
