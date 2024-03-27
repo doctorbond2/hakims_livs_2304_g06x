5ed91c15c81a9341199ba4a87d5eaa313de2f1d3
doctorbond2 <michael.rostlund@hotmail.com>
2024-03-27 13:31:49 +0100

    fafafa

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,22 +1,11 @@
 import express from "express";
-import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
-/* app.use(
-    cors({
-        origin: ["http://localhost:3000", "http://localhost:3001"],
-    })
-);
- */
 app.get("/", (req, res) => {
-  res.json({
-    connection: `PORT: ${process.env.PORT} \nMONGODB_URI: ${process.env.MONGODB_URI}`,
-  });
+  res.send("hi there");
 });
-
 export default app;
