69586c3e3ade28f60458f1017d598537aa37a1a1
doctorbond2 <michael.rostlund@hotmail.com>
2024-03-27 13:21:44 +0100

    vercel again

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,5 @@
 import app from "./app.js";
-import { RUN_SERVER } from "./config/dbconfig.js";
+// import { RUN_SERVER } from "./config/dbconfig.js";
 const PORT = process.env.PORT || 3000;
 const MONGODB_URI =
   process.env.MONGODB_URI || "mongodb://localhost:27017/hakims_livs";
