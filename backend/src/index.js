88d7f9a57939fb4f12655819aa161571d8e7b9ab
doctorbond2 <michael.rostlund@hotmail.com>
2024-03-27 14:56:07 +0100

    testing without vercel file

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import app from "./app.js";
 import { RUN_SERVER } from "./config/dbConfig.js";
+console.log(12);
 const PORT = process.env.PORT || 3000;
 const MONGODB_URI =
   process.env.MONGODB_URI || "mongodb://localhost:27017/hakims_livs";
