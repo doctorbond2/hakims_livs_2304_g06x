dfd8adb7025b7cf53172bab20bd1f406eaaed377
doctorbond2 <michael.rostlund@hotmail.com>
2024-03-27 12:00:30 +0100

    changed PORT env variable for connecting to express.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,13 @@
 import app from "./app.js";
 import { connectToMongoDB } from "./config/dbconfig.js";
-
+const PORT = process.env.PORT || 3000;
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-
 const run = async () => {
   await connectToMongoDB();
-  app.listen(3000, () => {
+  app.listen(PORT, () => {
     console.log("Server is running on port 3000");
   });
 };
