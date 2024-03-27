d9e2af11136a33da9e79d6904d1eafc3e9193a6e
doctorbond2 <michael.rostlund@hotmail.com>
2024-03-27 12:04:49 +0100

    added cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,13 +6,12 @@ const app = express();
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
-
+app.use(cors());
 /* app.use(
     cors({
         origin: ["http://localhost:3000", "http://localhost:3001"],
     })
 );
  */
-
 
 export default app;
