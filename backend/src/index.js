2c0fb71351804b685f8f6a9e69ca011bdc7dbae9
doctorbond2 <michael.rostlund@hotmail.com>
2024-03-27 13:16:40 +0100

    trying to connect to vercel

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,4 +3,7 @@ import { RUN_SERVER } from "./config/dbconfig.js";
 const PORT = process.env.PORT || 3000;
 const MONGODB_URI =
   process.env.MONGODB_URI || "mongodb://localhost:27017/hakims_livs";
-RUN_SERVER(MONGODB_URI, PORT);
+// RUN_SERVER(MONGODB_URI, PORT);
+app.listen(process.env.PORT, () => {
+  console.log("Connected to host:", process.env.PORT);
+});
