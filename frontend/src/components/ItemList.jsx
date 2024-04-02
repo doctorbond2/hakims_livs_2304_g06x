06ae84f98a25e0c918908a3fc7771f3ab6061fef
doctorbond2 <michael.rostlund@hotmail.com>
2024-04-02 16:15:10 +0200

    Co-authored-by: Inclusit <Inclusit@users.noreply.github.com> Co-authored-by: oablom <oablom@users.noreply.github.com>

--- frontend/src/components/ItemList.jsx
+++ frontend/src/components/ItemList.jsx
@@ -1,16 +1,27 @@
-const ItemList = ({ productList, aNumber }) => {
-  console.log(aNumber);
+const ItemList = ({ productList }) => {
   return (
     <>
       <h1>Item List</h1>
       <div>
         {productList &&
           productList.map((product) => (
-            <div key={product.id}>
-              <h2>{product.name}</h2>
-              <p>{product.description}</p>
-              <p>{product.price}</p>
-            </div>
+            <>
+              <div
+                key={product.id}
+                style={{
+                  border: "2px solid black",
+                  backgroundColor: "green",
+                  color: "white",
+                  width: "30vw",
+                  borderRadius: "4%",
+                }}
+              >
+                <h2>{product.name}</h2>
+                <p>{product.stock}</p>
+                <p>{product.price}</p>
+              </div>
+              <br></br>
+            </>
           ))}
       </div>
     </>
