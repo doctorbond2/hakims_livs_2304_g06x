0424f048e85d317a11aa88623c2bcaf7390f96ff
oablom <oablom@gmail.com>
2024-04-02 16:11:30 +0200

    admin page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,14 @@ import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
+import Admin from "./pages/Admin";
 function App() {
   return (
     <>
       <Header />
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/admin" element={<Admin />}></Route>
       </Routes>
       <Footer />
     </>
---
+++ frontend/src/pages/Admin.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import { Input } from "@/components/ui/input";
+
+export default function Admin() {
+  const [productName, setProductName] = useState("");
+  const [productPrice, setProductPrice] = useState("");
+  const [productStock, setProductStock] = useState("");
+
+  function addProductToMongoDB() {
+    console.log("Adding product to MongoDB");
+  }
+
+  function handleProductNameChange(e) {
+    setProductName(e.target.value);
+  }
+
+  function handleProductPriceChange(e) {
+    setProductPrice(e.target.value);
+  }
+
+  function handleProductStockChange(e) {
+    setProductStock(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
+  return (
+    <>
+      <form>
+        <label HTMLfor="productName"> Input product Name</label>
+        <input
+          id="productName"
+          type="text"
+          value={productName}
+          onChange={handleProductNameChange}
+        />
+        <br />
+        <label HTMLfor="productPrice">Product price</label>
+        <input
+          id="productPrice"
+          type="text"
+          value={productPrice}
+          onChange={handleProductPriceChange}
+        />{" "}
+        <br />
+        <label HTMLfor="productStock">Product stock: </label>
+        <input
+          id="productStock"
+          type="text"
+          value={productStock}
+          onChange={handleProductStockChange}
+        />{" "}
+        <br />
+      </form>
+    </>
+  );
+}
