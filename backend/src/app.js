05f683666ca4b1adff1d6186ee62d0c54f162409
Inclusit <camillefranzen@gmail.com>
2024-04-02 16:57:53 +0200

    small changes

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,7 +3,6 @@ import userRouter from "./routes/user.route.js";
 import productRouter from "./routes/product.route.js";
 import cors from "cors";
 const app = express();
-
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
