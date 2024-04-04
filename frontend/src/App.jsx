6743f119c8b2afda089f75a847c9714f59098d63
David Pensjö <David.pensjo@gmail.com>
2024-04-04 16:30:51 +0200

    Folder structure update for header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
+import Header from "./components/header/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Admin from "./pages/Admin";
