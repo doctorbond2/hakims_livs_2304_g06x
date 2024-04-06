a276975b10b5d913210634142bc7858534defeaf
DavidPensjo <david.pensjo@gmail.com>
2024-04-06 20:03:37 +0200

    pushtopull

--- frontend/src/components/productList/productCards/ProductCard.jsx
+++ frontend/src/components/productList/productCards/ProductCard.jsx
@@ -1,15 +1,13 @@
 import * as shad from "@/components/ui/shadBarrel";
 import ProductCardBody from "./ProductCardBody";
-
+import ProductModal from "./ProductModal";
 const ProductCard = ({ product }) => {
   return (
     <>
       <shad.Card className="w-[250px] h-[380px] grid grid-rows-1 border-slate-300">
         <shad.Dialog>
           <shad.DialogContent className="sm:max-w-[600px]">
-            <shad.Card className="w-[250px] h-[380px] border-slate-300">
-              <ProductCardBody product={product} />
-            </shad.Card>
+              <ProductModal product={product} />
           </shad.DialogContent>
           <shad.DialogTrigger asChild>
             <a>
--- frontend/src/components/productList/productCards/ProductModal.jsx
+++ frontend/src/components/productList/productCards/ProductModal.jsx
@@ -1,5 +1,19 @@
 import * as shad from "@/components/ui/shadBarrel";
 
 export default function ProductModal({ product }) {
-  return <></>;
+  return (
+    <>
+      <div className="grid grid-cols-2">
+        <div className="flex items-center justify-center size-96 pr-10">
+          <img className="size-auto" src="/powerking.jpg" alt="HakimLogo" />
+        </div>
+        <div className="flex flex-col ">
+          <h2 className="text-3xl">{product.name}</h2>
+          <shad.CardDescription className="">{product.name}</shad.CardDescription>
+          <shad.CardTitle className="text-red-500">{product.price}:-</shad.CardTitle>
+          <shad.Button className="">Köp</shad.Button>
+        </div>
+      </div>
+    </>
+  );
 }
