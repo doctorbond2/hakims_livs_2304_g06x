ab52f20ed9428b67b14be0cf9d24030f9ead65d6
Inclusit <camillefranzen@gmail.com>
2024-04-15 16:38:20 +0200

    orderlist fix

--- backend/src/routes/order.route.js
+++ backend/src/routes/order.route.js
@@ -1,3 +1,9 @@
 import express from "express";
+import { getOrderList, getOrderById } from "../controllers/order.controller.js";
 
 const router = express.Router();
+
+router.get("/", getOrderList);
+router.get("/:id", getOrderById);
+
+export default router;
