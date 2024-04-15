59fc8a9b800e53b3354dd5201980f094c921f5fd
Inclusit <camillefranzen@gmail.com>
2024-04-15 19:51:08 +0200

    sure

--- backend/src/routes/order.route.js
+++ backend/src/routes/order.route.js
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
-import express from "express";
 import { getOrderList, getOrderById, createOrder } from "../controllers/order.controller.js";
-
-=======
-import { getOrderList, getOrderById } from "../controllers/order.controller.js";
 import express from "express";
->>>>>>> 1588f66aae27c0daa3ce7cb4584f69f6a02d6370
 const router = express.Router();
 
 router.get("/", getOrderList);
