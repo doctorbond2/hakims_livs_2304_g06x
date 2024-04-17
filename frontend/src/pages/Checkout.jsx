f5e2b4b5baa7ba1f26aedcb8a3011a7aef2a823e
oablom <oablom@gmail.com>
2024-04-17 09:12:06 +0200

    Refactor CheckoutTable component and add CheckoutForm to Checkout page

--- frontend/src/pages/Checkout.jsx
+++ frontend/src/pages/Checkout.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import Page from "@/components/checkout/page";
+import CheckoutForm from "@/components/checkout/CheckoutForm";
 
 export default function Checkout() {
   return (
     <div className="flex justify-center">
-      <Page className="grid grid-cols-2 grid-rows-1"/>
+      <Page className="grid grid-cols-2 grid-rows-1" />
+      <CheckoutForm />
     </div>
   );
 }
