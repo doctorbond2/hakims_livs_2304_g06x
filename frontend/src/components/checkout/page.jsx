d50fca6923369d08106f67c581f6c6313f968b94
oablom <oablom@gmail.com>
2024-04-17 22:05:46 +0200

    Refactor package.json and Checkout components, and add Zustand dependency

--- frontend/src/components/checkout/page.jsx
+++ frontend/src/components/checkout/page.jsx
@@ -1,35 +1,15 @@
 import React, { useState, useEffect } from "react";
 import { columns } from "./columns";
 import { CheckoutTable } from "./checkoutTable";
-
-function getData() {
-  const cartData = JSON.parse(localStorage.getItem("shoppingCart")) || [];
-
-  const transformedData = cartData.map((item) => ({
-    id: item._id,
-    image: item.image.url,
-    productName: item.title,
-    amount: item.cartQuantity,
-    totalPrice: (item.discountedPrice * item.cartQuantity).toFixed(2),
-  }));
-
-  return Promise.resolve(transformedData);
-}
+// import useIsSubmitted from "./IsSubmitted";
+// import { useCart } from "../header/shoppingCart/CartContext";
 
 function tableContent() {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    getData().then((fetchedData) => {
-      setData(fetchedData);
-    });
-  }, []);
-
   return (
     <div className="container mx-auto py-10 grid grid-cols-2">
-      <CheckoutTable columns={columns} data={data} />
+      <CheckoutTable />
 
-      <h1>hello</h1>
+      {/* <h1>hello</h1> */}
     </div>
   );
 }
--- frontend/src/pages/Checkout.jsx
+++ frontend/src/pages/Checkout.jsx
@@ -1,12 +1,25 @@
 import React, { useState } from "react";
 import Page from "@/components/checkout/page";
 import CheckoutForm from "@/components/checkout/CheckoutForm";
+import { useEffect } from "react";
 
 export default function Checkout() {
+  const [emptyCart, setEmptyCart] = useState(false);
+
+  function checkCart() {
+    if (localStorage.getItem("shoppingCart") === null) {
+      setEmptyCart(true);
+    }
+  }
+
+  useEffect(() => {
+    checkCart();
+  }, []);
+
   return (
-    <div className="flex justify-center">
+    <div className="">
       <Page className="grid grid-cols-2 grid-rows-1" />
-      <CheckoutForm />
+      {!emptyCart && <CheckoutForm />}
     </div>
   );
 }
