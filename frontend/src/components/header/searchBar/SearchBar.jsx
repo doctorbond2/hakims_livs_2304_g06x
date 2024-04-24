5d20f4e93e02a04ffad58219499ab9ac284642ce
DavidPensjo <david.pensjo@gmail.com>
2024-04-24 03:24:35 +0200

    Update Header component styles and add new utility classes in Tailwind config FixedSearchBar

--- frontend/src/components/header/searchBar/SearchBar.jsx
+++ frontend/src/components/header/searchBar/SearchBar.jsx
@@ -1,129 +1,106 @@
-import React, { useState, useEffect, useRef } from "react";
-import axios from "axios";
-import { debounce } from "lodash";
-import ReactDOM from "react-dom";
+import React from "react";
+import { useState, useEffect } from "react";
+import { GET_REQUEST } from "@/utils/helpers/request.helper";
 import * as shad from "@/components/ui/shadBarrel";
 import ProductModal from "@/components/productList/productCards/ProductModal";
-import { Cross1Icon } from "@radix-ui/react-icons";
+import { Cross1Icon, MagnifyingGlassIcon } from "@radix-ui/react-icons";
 
-const SearchBar = () => {
-  const [query, setQuery] = useState("");
-  const [products, setProducts] = useState([]);
-  const [error, setError] = useState("");
-  const [portalPosition, setPortalPosition] = useState({ top: 0, left: 0, width: 0 });
-  const [showPortal, setShowPortal] = useState(false);
-  const inputRef = useRef(null);
-  const portalRef = useRef(null);
-  const verticalOffset = 10;
-
-  const searchProducts = debounce(async (query) => {
-    if (!query) {
-      setProducts([]);
-      return;
-    }
-    try {
-      const response = await axios.get(`${import.meta.env.VITE_BaseUrl}/api/search?query=${encodeURIComponent(query)}`);
-      setProducts(response.data.length > 0 ? response.data : []);
-    } catch (err) {
-      setError("Failed to fetch products. Please try again.");
-      console.error("Error fetching search results", err);
-    }
-  }, 300);
-
+function SearchBar() {
   useEffect(() => {
-    searchProducts(query);
-  }, [query]);
-
-  const calculatePortalPosition = () => {
-    const inputField = inputRef.current;
-    if (inputField) {
-      const inputRect = inputField.getBoundingClientRect();
-      setPortalPosition({
-        top: inputRect.bottom + verticalOffset,
-        left: inputRect.left,
-        width: inputRect.width,
-      });
-    }
-  };
-
-  useEffect(() => {
-    calculatePortalPosition();
-    window.addEventListener("resize", calculatePortalPosition);
-    return () => {
-      window.removeEventListener("resize", calculatePortalPosition);
-    };
-  }, []);
-
-  useEffect(() => {
-    const handleClick = (event) => {
-      if (portalRef.current && !portalRef.current.contains(event.target)) {
-        setShowPortal(false);
+    const fetchData = async () => {
+      let endpoint = "/api/products";
+      try {
+        const products = await GET_REQUEST(endpoint);
+        if (products) {
+          setProducts(products);
+        }
+      } catch (err) {
+        console.error(err.message);
       }
     };
+    fetchData();
+  }, []);
 
-    if (showPortal) {
-      document.addEventListener("mousedown", handleClick);
+  const [products, setProducts] = useState([]);
+  const [queryEntered, setQueryEntered] = useState("");
+  const [filteredProducts, setFilteredProducts] = useState([]);
+
+  const handleFilter = (e) => {
+    const query = e.target.value.toLowerCase();
+    setQueryEntered(query);
+    const newFilter = products.filter((product) => {
+      return product.title.toLowerCase().includes(query);
+    });
+
+    if (query === "") {
+      setFilteredProducts([]);
+      return;
     } else {
-      document.removeEventListener("mousedown", handleClick);
+      setFilteredProducts(newFilter);
     }
-
-    return () => {
-      document.removeEventListener("mousedown", handleClick);
-    };
-  }, [showPortal]);
-
-  const handleInputFocus = () => {
-    setShowPortal(true);
   };
 
-  const handleClearInput = () => {
-    setQuery("");
-    inputRef.current.focus();
+  const clearInput = () => {
+    setFilteredProducts([]);
+    setQueryEntered("");
   };
 
-  const clearButton = query.length > 0 && (
-    <button className="-ml-6 font-bold" onClick={handleClearInput}>
-      <Cross1Icon />
-    </button>
-  );
-
-  const resultsDropdown = (
-    <div ref={portalRef} style={{ position: "absolute", top: portalPosition.top, left: portalPosition.left, width: portalPosition.width }}>
-      {error ? (
-        <p style={{ color: "red" }}>{error}</p>
-      ) : (
-        products.map((product) => (
-          <shad.Dialog key={product.id}>
-            <shad.DialogTrigger asChild>
-              <a className="grid bg-slate-50 items-end border grid-cols-7 grid-rows-2 cursor-pointer">
-                <div className="flex justify-center row-span-2 col-span-1 ">
-                  <img className="max-w-full max-h-20 object-contain p-1" src={product.image.url} alt={product.image.alt || "Product Image"} />
-                </div>
-                <h3 className="text-2xl row-span-1 col-span-6 row-start-1 col-start-2">{product.title}</h3>
-                <h4 className="text-lg font-semibold col-span-6 row-start-2 col-start-2 pb-1">{product.brand}</h4>
-                <h4 className="text-base font-normal col-span-6 row-start-2 col-start-3 pb-1">{product.category.title}</h4>
-              </a>
-            </shad.DialogTrigger>
-            <shad.DialogContent className="sm:max-w-[800px] grid grid-cols-2 grid-rows-2">
-              <ProductModal product={product} />
-            </shad.DialogContent>
-          </shad.Dialog>
-        ))
+  return (
+    <div className="">
+      <div className="flex">
+        <input
+          type="text"
+          placeholder="Sök..."
+          className="border-0 bg-white rounded-md rounded-tr-none rounded-br-none text-lg h-[60px] w-[350px] p-4  focus:outline-none"
+          value={queryEntered}
+          onChange={handleFilter}
+        />
+        <div className="h-[60px] w-[50px] bg-white grid place-items-center rounded-md rounded-tl-none rounded-bl-none">
+          {queryEntered.length === 0 ? (
+            <MagnifyingGlassIcon width="26" height="26" />
+          ) : (
+            <Cross1Icon
+              width="20"
+              height="20"
+              className="cursor-pointer w-max h-max"
+              onClick={clearInput}
+            />
+          )}
+        </div>
+      </div>
+      {filteredProducts.length != 0 && (
+        <div className="overflow-hidden overflow-y-auto h-[272px] w-[400px] no-scrollbar absolute rounded-sm">
+          {filteredProducts.slice(0, 4).map((product) => (
+            <shad.Dialog key={product.id}>
+              <shad.DialogTrigger asChild>
+                <a className="grid bg-slate-50 items-end border grid-cols-7 grid-rows-2 cursor-pointer">
+                  <div className="flex justify-center row-span-2 col-span-1 ">
+                    <img
+                      className="max-w-full max-h-20 object-contain p-1"
+                      src={product.image.url}
+                      alt={product.image.alt || "Product Image"}
+                    />
+                  </div>
+                  <h3 className="text-2xl row-span-1 col-span-6 row-start-1 col-start-2">
+                    {product.title}
+                  </h3>
+                  <h4 className="text-lg font-semibold col-span-6 row-start-2 col-start-2 pb-1">
+                    {product.brand}
+                  </h4>
+                  <span className="text-base font-normal col-span-6 row-start-2 col-start-auto pb-1 pr-1">
+                    {product.category.title}
+                  </span>
+                </a>
+              </shad.DialogTrigger>
+              <shad.DialogContent className="sm:max-w-[800px] grid grid-cols-2 grid-rows-2">
+                <ProductModal product={product} />
+              </shad.DialogContent>
+            </shad.Dialog>
+          ))}
+        </div>
       )}
     </div>
   );
-
-  return (
-    <div className="relative">
-      <form onSubmit={(e) => e.preventDefault()}>
-        <div className="flex">
-          <shad.Input ref={inputRef} type="text" placeholder="Sök efter produkter..." value={query} onChange={(e) => setQuery(e.target.value)} onFocus={handleInputFocus} />
-          {clearButton}
-        </div>
-      </form>
-      {showPortal && ReactDOM.createPortal(resultsDropdown, document.getElementById("portal-root"))}
-    </div>
-  );
-};
+}
 
 export default SearchBar;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -74,5 +74,20 @@ module.exports = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [
+    require("tailwindcss-animate"),
+    function ({ addUtilities }) {
+      const newUtilities = {
+        ".no-scrollbar::-webkit-scrollbar": {
+          display: "none",
+        },
+        ".no-scrollbar": {
+          "-ms-overflow-style": "none",
+          "scrollbar-width": "none",
+        },
+      };
+
+      addUtilities(newUtilities);
+    },
+  ],
 };
