08e9820137d69cc533ccb500f20bb766b0a46f11
doctorbond2 <michael.rostlund@hotmail.com>
2024-05-22 08:33:58 +0200

    chore: Add Docker configuration for backend and frontend services

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,21 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
+  },
+  server: {
+    host: true,
+    port: 8080,
+  },
+  preview: {
+    host: true,
+    port: 8080,
   },
 });
