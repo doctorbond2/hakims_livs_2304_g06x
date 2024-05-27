1bc450f4a41ef4d196752c5754984776681b3369
doctorbond2 <michael.rostlund@hotmail.com>
2024-05-27 16:12:45 +0200

    chore: Refactor LoginRegister component in LoginRegister.jsx

--- frontend/src/components/login/LoginRegister.jsx
+++ frontend/src/components/login/LoginRegister.jsx
@@ -1,23 +1,26 @@
-import * as shad from "@/components/ui/shadBarrel";
-import React, { useEffect } from "react";
-import LoginRegisterInput from "./LoginRegisterInput";
-import { useState } from "react";
+import * as shad from '@/components/ui/shadBarrel';
+import React, { useEffect } from 'react';
+import LoginRegisterInput from './LoginRegisterInput';
+import { useState } from 'react';
 const LoginRegister = ({ register }) => {
   const defaultRegisterState = {
-    firstName: "",
-    lastName: "",
-    email: "",
-    username: "",
-    password: "",
-    password_again: "",
+    firstName: '',
+    lastName: '',
+    email: '',
+    username: '',
+    password: '',
+    password_again: '',
   };
   const [registerData, setregisterData] = useState(defaultRegisterState);
   const submitRegister = async (e) => {
     e.preventDefault();
     try {
       if (registerData.password === registerData.password_again) {
-        await register(registerData);
-        setregisterData(defaultRegisterState);
+        const response = await register(registerData);
+        if (response) {
+          setregisterData(defaultRegisterState);
+          alert('Welcome to Hakim Online!');
+        }
       }
     } catch (err) {
       console.log(err.message);
@@ -37,10 +40,12 @@ const LoginRegister = ({ register }) => {
         </shad.CardHeader>
         <form onSubmit={submitRegister}>
           <shad.CardContent className="space-y-2">
-            <LoginRegisterInput {...{ registerData, setregisterData }} />
+            {registerData && (
+              <LoginRegisterInput {...{ registerData, setregisterData }} />
+            )}
           </shad.CardContent>
           <shad.CardFooter>
-            <shad.Button type={"submit"} className="bg-green-800">
+            <shad.Button type={'submit'} className="bg-green-800">
               Skapa konto!
             </shad.Button>
           </shad.CardFooter>
